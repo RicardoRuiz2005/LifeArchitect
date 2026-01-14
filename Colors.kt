4ff39c49a332107dd76d106ed2d44dfd111c01a5
RicardoRuiz2005 <150260615+RicardoRuiz2005@users.noreply.github.com>
2026-01-14 17:12:03 -0600

    Create Colors.kt

---
+++ Colors.kt
@@ -0,0 +1,8 @@
+package com.example.lifearchitect
+
+import androidx.compose.ui.graphics.Color
+
+val GrassGreen = Color(0xFF7CFC00)
+val CharcoalGrey = Color(0xFF2B2B2B)
+val DarkAmber = Color(0xFFFFCC00)
+val CyanBlue = Color(0xFF00FFFF)
